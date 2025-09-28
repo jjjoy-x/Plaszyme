be1ee2665acf731179efd37f5b017b1a706497c3
shulei <Shuleihe@outlook.com>
2025-09-29 02:01:23 +0800

    refactor: delete other files

--- test/test_structure_encoder.py
+++
@@ -1,7 +0,0 @@
-from features.structure_encoder import StructureEncoder
-
-encoder = StructureEncoder()
-seq = 'ACDEFGHIKL'
-
-adj = encoder('/Users/shulei/PycharmProjects/Plaszyme/test/test_strc.pdb',seq)
-print(adj)
